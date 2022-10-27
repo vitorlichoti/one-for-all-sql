75b0ed52a0fd24764b66c0be4e3877a94c6b0313
Vitor Lichoti <vitorlichoti@pop-os.localdomain>
2022-10-27 13:45:40 -0400

    Requisito 11 finalizado

--- desafio11.sql
+++ desafio11.sql
@@ -1,9 +1,7 @@
-SELECT nome_cancao AS nome_musica,
+SELECT nome_cancao as nome_musica,
 REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(nome_cancao, 'Bard', 'QA'), 'Amar', 'Code Review'), 'Pais', 'Pull Requests'),'SOUL', 'CODE'), 'SUPERSTAR', 'SUPERDEV') AS novo_nome
 FROM SpotifyClone.tabela_cancoes
-WHERE nome_cancao LIKE '%Bard%'
-   OR nome_cancao LIKE '%Amar%'
-   OR nome_cancao LIKE '%Pais%'
-   OR nome_cancao LIKE '%SOUL%'
+WHERE nome_cancao LIKE '%Bard%' OR nome_cancao LIKE '%Amar%'
+   OR nome_cancao LIKE '%Pais%' OR nome_cancao LIKE '%SOUL%'
    OR nome_cancao LIKE '%SUPERSTAR%'
 ORDER BY nome_musica DESC;
